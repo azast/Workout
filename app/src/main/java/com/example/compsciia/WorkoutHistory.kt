08e10878a2a41456a292b54d35d92d1287f113f3
Azast <petteri.hokka@icloud.com>
2023-11-28 02:14:00 +0200

    Added more features

--- app/src/main/java/com/example/compsciia/WorkoutHistory.kt
+++ app/src/main/java/com/example/compsciia/WorkoutHistory.kt
@@ -1,4 +1,35 @@
 package com.example.compsciia
 
-class WorkoutHistory {
-}
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.compsciia.Models.Workout
+import com.example.compsciia.databinding.ActivityAddWorkoutBinding
+import com.example.compsciia.databinding.ActivityWorkoutHistoryBinding
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class WorkoutHistory : AppCompatActivity() {
+    private lateinit var binding: ActivityWorkoutHistoryBinding
+
+    private lateinit var workout: Workout
+
+    private lateinit var old_workout: Workout
+
+    var isUpdate = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWorkoutHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgBackArrow.setOnClickListener {
+            onBackPressed()
+
+            }
+        }
+    }
